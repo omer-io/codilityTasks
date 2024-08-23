f2c80640a9cfe6f5bad69859688d5299821e89b8
omer-io <omer.jamil@rakurai.io>
2024-08-23 11:19:30 +0500

    fixed comments

--- lesson5_2/lesson5_2.cpp
+++ lesson5_2/lesson5_2.cpp
@@ -29,23 +29,6 @@ int solution(vector<int> &A){
     }
 
     return minIndex;
-    // int n = A.size();
-    // float minAvg = INT_MAX;
-    // int startPos;
-    // for(int i = 0; i < n-1; i++){
-    //     for(int j = i + 1; j < n; j++){
-    //         float sum = 0;
-    //         for(int k = i; k <= j; k++){
-    //             sum += A[k];
-    //         }
-    //         sum =  sum / (j - i + 1);
-    //         if(sum < minAvg){
-    //             minAvg = sum;
-    //             startPos = i;
-    //         }
-    //     }
-    // }
-    // return startPos;
 }
 
 
